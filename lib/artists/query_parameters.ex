87964702ae8e73e4f86099ab3487761dd855ac98
Derrick Plotsky <derrickplotsky@gmail.com>
2019-03-19 12:51:10 -0700

    Add functions for handling query parameters
    Some of the query parameters for querying artists are a little annoying,
    and so this adds some helpers specifically around tags and artist name.

---
+++ lib/artists/query_parameters.ex
@@ -0,0 +1,30 @@
+defmodule Elixibop.Artists.QueryParameters do
+  def queryize_name(name) do
+    name
+    |> URI.encode_www_form()
+    |> (&("artist:" <> &1)).()
+  end
+
+  def build_url_params(name_query, "") do
+    name_query
+  end
+
+  def build_url_params(name_query, tag_query) do
+    (name_query <> URI.encode_www_form(" AND ") <> tag_query)
+  end
+
+  def queryize_genres(genres) do
+    genres
+    |> Enum.map(fn genre -> "tag:" <> URI.encode_www_form(genre) end)
+    |> Enum.join(URI.encode_www_form(" OR "))
+    |> final_tag
+  end
+
+  def final_tag("") do
+    ""
+  end
+
+  def final_tag(query) do
+    "(" <> query <> ")"
+  end
+end
