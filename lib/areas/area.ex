4ba863bdf99ddfc99a948d1a6fda98b3fe14db9c
Derrick Plotsky <derrickplotsky@gmail.com>
2019-03-24 18:23:35 -0700

    Add builder for Area
    This code takes in a map, and then creates a struct from it.
    
    I'm also removing a different iso code struct because it didn't really
    make sense to use.

--- lib/areas/area.ex
+++ lib/areas/area.ex
@@ -17,17 +17,45 @@ defmodule Elixibop.Areas.Area do
 
   def from_map(area_map) do
     %Elixibop.Areas.Area{
-      id: area_map.id,
-      type: area_map.type,
-      name: area_map.name,
-      sort_name: area_map.sort_name,
-      life_span: area_map.life_span
+      id: area_map[:id],
+      type: area_map[:type],
+      name: area_map[:name],
+      score: area_map[:score],
+      sort_name: area_map[:sort_name],
+      life_span: area_map[:life_span],
+      relation_list: build_relation_list(area_map[:relation_list]),
+      alias_list: area_map[:alias_list],
+      iso_3166_2_code_list: build_code_list(area_map[:iso_3166_2_code_list])
     }
   end
-end
 
-defmodule Elixibop.IsoCodeList do
-  defstruct ~w[iso_3166_2_code]a
+  def build_relation_list(nil) do
+    []
+  end
+
+  def build_relation_list(list) do
+    Enum.map(list, fn relation -> relation_from_map(relation) end)
+  end
+
+  def build_code_list(nil) do
+    []
+  end
+
+  def build_code_list(list) do
+    Enum.map(list, fn item -> item[:code] end)
+  end
+
+  def relation_from_map(nil) do
+    %{}
+  end
+
+  def relation_from_map(relation_map) do
+    %{
+      target: relation_map[:target],
+      direction: relation_map[:direction],
+      area: from_map(relation_map[:area])
+    }
+  end
 end
 
 defmodule Elixibop.Alias do
