40fa45dcf952d825d0a08a23d66a40bbcccd00e9
Derrick Plotsky <derrickplotsky@gmail.com>
2019-04-02 20:29:32 -0700

    Add functions for query options
    We want to turn the query options into url paramters, and now we have
    that ability. It calls a function for each, and then filters down to
    only the ones that exist, and joins them all together with "AND". In the
    future maybe I'll add the ability for "OR"

--- lib/artists/query_parameters.ex
+++ lib/artists/query_parameters.ex
@@ -1,7 +1,18 @@
+alias Elixibop.Artists.QueryOptions
+
 defmodule Elixibop.Artists.QueryParameters do
-  def queryize_name(name) do
-    name
-    |> URI.encode_www_form()
+  def build_url_params(%QueryOptions{} = options) do
+    IO.puts(inspect(options))
+    params = [
+      queryize_id(options.id),
+      queryize_name(options.name),
+      queryize_genres(options.tags),
+      queryize_type(options.type),
+      queryize_country(options.country)
+    ]
+    IO.puts(inspect(params))
+    filtered = Enum.reject(params, &nil?/1)
+    join_params(filtered)
   end
 
   def build_url_params(name_query, "") do
@@ -10,6 +21,48 @@ defmodule Elixibop.Artists.QueryParameters do
 
   def build_url_params(name_query, tag_query) do
     (name_query <> URI.encode_www_form(" AND ") <> tag_query)
+  end
+
+  def queryize_name(nil) do
+    nil
+  end
+
+  def queryize_name(name) do
+    name
+    |> URI.encode_www_form()
+    |> (&("artistaccent:" <> &1)).()
+  end
+
+  def join_params(params) do
+    Enum.join(params, URI.encode_www_form(" AND "))
+  end
+
+  def queryize_id(nil) do
+    nil
+  end
+
+  def queryize_id(id) do
+    "arid:#{id}"
+  end
+
+  def queryize_type(nil) do
+    nil
+  end
+
+  def queryize_type(type) do
+    "type:#{type}"
+  end
+
+  def queryize_country(nil) do
+    nil
+  end
+
+  def queryize_country(country) do
+    "country:#{country}"
+  end
+
+  def queryize_genres(nil) do
+    nil
   end
 
   def queryize_genres(genres) do
@@ -25,5 +78,13 @@ defmodule Elixibop.Artists.QueryParameters do
 
   def final_tag(query) do
     "(" <> query <> ")"
+  end
+
+  def nil?(nil) do
+    true
+  end
+
+  def nil?(_arg) do
+    false
   end
 end
